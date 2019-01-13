7944e0c175db9ff9941cab091f8a0ffc52193c94
J <jennifer.singleton@gmail.com>
2019-01-13 17:02:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  array.each do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  room = 1
+  speakers.each do |name|
+    puts "Hello, #{name}! You'll be assigned to room #{room}!"
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+end
