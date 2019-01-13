2b1c4f11172d021ede3a994db2d0db54294cb248
J <jennifer.singleton@gmail.com>
2019-01-13 17:14:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,11 +23,11 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(speakers).each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
 
-  assign_rooms(speakers).each do |room|
+  assign_rooms(attendees).each do |room|
     puts room
   end
 end
